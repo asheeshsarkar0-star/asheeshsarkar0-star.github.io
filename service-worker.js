d0c977c287d6cdbe454be599348dfebe075b5979
Asheesh Sarkar <asheeshsarkar0@gmail.com>
2026-01-14 18:06:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = "asheesh-cache-v2";
+const CACHE_NAME = "asheesh-cache-v3";
+
 const ASSETS = [
   "/",
   "/index.html",
+  "/offline.html",
   "/manifest.json",
   "/icon-192.png",
   "/icon-512.png",
 
-  // Images (slides + gallery)
+  // Images
   "https://raw.githubusercontent.com/asheeshsarkar0-star/asheeshsarkar0-star.github.io/main/file_00000000650c7209b41d602902782a9a.png",
   "https://raw.githubusercontent.com/asheeshsarkar0-star/asheeshsarkar0-star.github.io/main/file_000000005034720792453fffe09544aa%20(1).png",
-
-  // Gallery images
   "https://raw.githubusercontent.com/asheeshsarkar0-star/asheeshsarkar0-star.github.io/main/file_0000000067c87207a6d265ab32f0a34c.png",
   "https://raw.githubusercontent.com/asheeshsarkar0-star/asheeshsarkar0-star.github.io/main/file_00000000862c71fa8ef54358a8a6c2ea.png",
 
@@ -28,22 +28,21 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching app assets...");
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Activate (delete old cache)
+// Activate (delete old caches)
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 });
 
@@ -53,9 +52,7 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => {
       return (
         cached ||
-        fetch(event.request).then((response) => {
-          return response;
-        })
+        fetch(event.request).catch(() => caches.match("/offline.html"))
       );
     })
   );
